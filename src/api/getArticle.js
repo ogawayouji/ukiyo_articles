49633a1cc56ef679d0f85bc86bc2598c5cf9609d
ogawayouji <satsumaimo697@gmail.com>
2023-03-13 17:01:51 +0900

    feat: データ取得

---
+++ src/api/getArticle.js
@@ -0,0 +1,16 @@
+export const getArticle = () => {
+  const endpoint = "/api/v1/articles"
+  const options = {
+    headers: {
+      "X-MICROCMS-API-KEY": process.env.X_MICROCMS_API_KEY
+    },
+    method: 'GET'
+  }
+
+  return new Promise((resolve, reject) => {
+    fetch(endpoint, options)
+      .then((res) => res.json())
+      .then((data) => resolve(data))
+      .catch(err => console.log(err))
+  })
+}
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getArticle } from '../api/getArticle'
 
 export const Articles = () => {
+  const [loading , setLoading] = useState(true)
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      let res = await getArticle()
+      setArticles(res.contents)
+      setLoading(false)
+    }
+    fetchData()
+  }, [])
+
   return (
     <div>
       <h1>Articles</h1>
-      articles
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>{articles.forEach(article => console.log(article))}</div>
+      )}
     </div>
   )
 }
